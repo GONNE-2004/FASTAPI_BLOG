fb2dbcdac991a2ada3418a8e0f03aa16a24acd09
GONNE <gonne411@gmail.com>
2026-06-09 09:24:33 +0200

    added template folder and static folder

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse # Import the FastAPI class and HTMLResponse from the fastapi module
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static" )
+
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
@@ -20,11 +25,14 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False) # Define a GET endpoint at the root URL and access it with the home function
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False) # Define a GET endpoint at /posts and access it with the home function
-def home():
-    return f"<h1>{posts[0]['title']}</h1>" # Return the title and content of the first post in the posts list as an HTML response
-
+@app.get("/", include_in_schema=False, name="home") # Define a GET endpoint at the root URL and access it with the home function
+@app.get("/posts", include_in_schema=False, name="posts") # Define a GET endpoint at /posts and access it with the home function
+def home(request : Request):
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"posts": posts, "title": "Home"}
+        )
 
 @app.get("/api/posts") # Define a GET endpoint at /api/posts and access it with the get_posts function
 def get_posts():
