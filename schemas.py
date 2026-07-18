1e730db7dac7f5f6ae3b4a2dd2d71597d4ac49be
GONNE <gonne411@gmail.com>
2026-07-18 08:00:33 +0200

    updated the schemas

--- schemas.py
+++ schemas.py
@@ -1,4 +1,22 @@
-from pydantic import ConfigDict, BaseModel, Field
+from datetime import datetime
+from pydantic import ConfigDict, BaseModel, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    image_file: str | None
+    image_path: str
 
 class PostBase(BaseModel):
   title: str = Field(min_length= 1, max_length= 100)
@@ -16,4 +34,6 @@ class PostResponse(PostBase):
   #use of type notation to access data
 
   id: int
-  date_posted: str
+  user_id: int
+  date_posted: datetime
+  author: UserResponse
